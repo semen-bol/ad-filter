e2d412ba907c7cb816a7931d04c1151f145950c7
NemoDev <82635729+semen-bol@users.noreply.github.com>
2024-09-08 15:51:44 +0500

    Update text_filter_ad.py

--- utils/text_filter_ad.py
+++ utils/text_filter_ad.py
@@ -1,6 +1,7 @@
 import asyncio
+import torch
 
-from transformers import pipeline
+from transformers import pipeline, AutoTokenizer, AutoModelForSequenceClassification
 
 # Инициализация модели для поиска схожих текстов
 classifier = pipeline("zero-shot-classification", model="joeddav/xlm-roberta-large-xnli")
@@ -12,8 +13,25 @@ async def find_similar_texts(text, candidate_labels=["scam", "spam", "phishing",
         best_label = result['labels'][0]
         best_score = result['scores'][0]
 
+        # b
+        best_score = round(best_score, 2)
+        best_score2 = str(best_score)
+        a,b = ([int(x)]  for x in best_score2.split(".",1))
+
         if best_score >= threshold:
-            return best_label, best_score
+            return best_label, b[0]
         else:
             return None, best_score
-    except TypeError as err: print(err); pass
+    except TypeError as err: pass
+
+model_path = "RUSpam/spam_deberta_v4"
+tokenizer = AutoTokenizer.from_pretrained(model_path)
+model = AutoModelForSequenceClassification.from_pretrained(model_path)
+
+def predict(text):
+    inputs = tokenizer(text, return_tensors="pt", truncation=True, max_length=256)
+    with torch.no_grad():
+        outputs = model(**inputs)
+        logits = outputs.logits
+        predicted_class = torch.argmax(logits, dim=1).item()
+    return True if predicted_class == 1 else False
